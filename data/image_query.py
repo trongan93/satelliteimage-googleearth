f30d58eb43bd1df4d5e2cfee8aa95cffe3d9c493
Trong-An Bui <trongan93@gmail.com>
2020-08-07 17:51:37 +0800

    satellite image query

--- data/image_query.py
+++ data/image_query.py
@@ -1,3 +1,11 @@
+import ee
+class SatelliteQueryImage:
+    def __init__(self):
+        ee.Authenticate()
+        ee.Initialize()
+    def getRGBSatelliteImagesContainLandslide(self, landslide_lat, landslide_lng, landslide_event_data, landslide_event_end):
+        rgbLandsatImages = ee.ImageCollection("LANDSAT/LC08/C01/T1").filterBounds(ee.Geometry.Point(landslide_lat,landslide_lng)).filterDate(landslide_event_data,landslide_event_end).sort('CLOUDY_PIXEL_PERCENTAGE')
+
 def querySatelliteImages(lat,lng, event_date):
 
     return 1
