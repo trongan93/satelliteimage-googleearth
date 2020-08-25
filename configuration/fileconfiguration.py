9716393cbab5560d15bc485286eda16fbeba1fa5
Trong-An Bui <trongan93@gmail.com>
2020-08-25 17:00:34 +0800

    update config

--- configuration/fileconfiguration.py
+++ configuration/fileconfiguration.py
@@ -2,4 +2,5 @@ from pathlib import Path
 base_path = Path().absolute()
 raw_glc_file = Path(base_path, 'raw', 'nasa_global_landslide_catalog_point.csv')
 short_glc_file = Path(base_path,'raw','short_glc.csv')
+# short_glc_file = Path(base_path,'raw','test_glc.csv')
 base_saved_data_path = "/mnt/d/ProjectData/Landslide_Google_Earth_Engine"
